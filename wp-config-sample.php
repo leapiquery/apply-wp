d0c3c05c63eb454f40db6a2de7743259009a457b
leapiquery <125121908+leapiquery@users.noreply.github.com>
2023-02-11 13:16:00 +0100

    Add files via upload

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -19,13 +19,13 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
-define('DB_NAME', 'votre_nom_de_bdd');
+define( 'DB_NAME', 'root' );
 
 /** Utilisateur de la base de données MySQL. */
-define('DB_USER', 'votre_utilisateur_de_bdd');
+define('DB_USER', 'root');
 
 /** Mot de passe de la base de données MySQL. */
-define('DB_PASSWORD', 'votre_mdp_de_bdd');
+define( 'DB_PASSWORD', '' );
 
 /** Adresse de l’hébergement MySQL. */
 define('DB_HOST', 'localhost');
@@ -49,14 +49,16 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('AUTH_KEY',         ' %8Y@*buv>Rrn`>-04&HAya?2]5i4ht#h++n{V>9-1Mq(K?ST~,c[+)d&Qs ~C0#');
+  define('SECURE_AUTH_KEY',  'bHW+bw]_Vf;2z27zXL-olz:&- sRStJy|7SuJ}g-L]14r so%_fEi=DwSwC-|.4I');
+  define('LOGGED_IN_KEY',    'N>0,6w`LKdji_37vKJ8D<[8/^9:*bjNs@iZa|qDH$XEd^G|#-5R/e[^6sxRUR@oV');
+  define('NONCE_KEY',        'VCGmd*4E!]oVcpRW;{-];-b2VUUa<w/O*Gutnn9ds.xTx8@L)c 8Udr9vV@nM+Ol');
+  define('AUTH_SALT',        'A{^8-.I3]$3X)(U^#F~/,b5P{w=3h=K$pdXo(!K`-D15>9&/Aqsh+Ag!pMq q2)9');
+  define('SECURE_AUTH_SALT', 'v2Ah%4.nd$;[w-uQ2j|o6S(a|)YbmgJ]+~EPJ^B:<bpk>G9WWeJpSoaJ0 8Q^<-`');
+  define('LOGGED_IN_SALT',   '@PA]YwG1T8VgYVG<)B3MQuxh*0_xg$r0HXKa.]w8=]G#F`{-[gvQN+:Ojc<es4iT');
+  define('NONCE_SALT',       '-*MzfQ<v9BG(UvrI4i+cA=z|b1*K*!8`a:kNM9vs3|GczfyWMKb+p?j4mM&qc1IA');
+  define( 'WP_HOME', 'http://apply-wp-master.com/wordpress/' );
+  define( 'WP_SITEURL', 'http://apply-wp-master.com/wordpress/' );
 /**#@-*/
 
 /**
